c5716f3b7257f1f577e2aa2176587133603cbe50
Perry Cate <NPCate01@gmail.com>
2017-03-22 15:59:14 -0400

    Add govendor
    I really don't like that we have to include all of the dependencies in
    the repo, but this is the way that the Heroku Go tutorial works and I'm
    not willing to try to fight this at the moment.

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"os"
 	"log"
-    "net/http"
+	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
@@ -25,5 +25,5 @@ func main() {
 		c.String(http.StatusOK, "Hello World!")
 	})
 
-    router.Run(":" + port)
+	router.Run(":" + port)
 }
