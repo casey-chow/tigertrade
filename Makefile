8ac3fcf37c03a83f5728f991a97612dbd4c4bae0
Casey Chow <casey.chow@gmail.com>
2017-04-06 21:55:12 -0400

    add go fmt

--- Makefile
+++ Makefile
@@ -5,5 +5,9 @@ default: run
 run:
 	revel run github.com/TheGuyWithTheFace/tigertrade
 
+# Simplifies, overwrites and prints the filename of any file with "bad" formatting.
+fmt:
+	gofmt -s -l -w .
+
 install:
 	govendor sync
--- app/init.go
+++ app/init.go
@@ -29,7 +29,6 @@ func init() {
 		revel.ActionInvoker,           // Invoke the action.
 	}
 
-
 	// register startup functions with OnAppStart
 	// revel.DevMode and revel.RunMode only work inside of OnAppStart. See Example Startup Script
 	// ( order dependent )
