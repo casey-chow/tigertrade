091784a40a3bed1e7d270b658e0707b9b3cb2043
Casey Chow <casey.chow@gmail.com>
2017-04-08 15:47:23 -0400

    remove bullshit, add swag

--- client/src/theme/bootstrap.config.js
+++ client/src/theme/bootstrap.config.js
@@ -11,8 +11,8 @@
  */
 
 module.exports = {
-  preBootstrapCustomizations: './src/theme/variables.scss',
-  mainSass: './src/theme/bootstrap.overrides.scss',
+  preBootstrapCustomizations: './client/src/theme/variables.scss',
+  mainSass: './client/src/theme/bootstrap.overrides.scss',
   verbose: false,
   debug: false,
   scripts: {
