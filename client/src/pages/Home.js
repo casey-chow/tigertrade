cdc818e1a1a61822345eca4ecd330159c79b07a5
Evan Wildenhain <mew2@princeton.edu>
2017-04-09 03:39:29 -0400

    Added react-grid-system, changed listing cards to inbox-style expandable view

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,33 +2,23 @@ import React, { Component } from 'react';
 import {GridList, GridTile} from 'material-ui/GridList';
 import ListingCard from './../components/ListingCard'
 import { dummyListings } from './../util/dummyData'
-//import './Home.css'
-
-const styles = {
-  root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
-  },
-  gridList: {
-    width: 700,
-  },
-};
-
+import { Container, Row, Col } from 'react-grid-system';
+import './Home.css'
 
 
 class Home extends Component {
   render() {
     return (
       <div className="Home">
-         <div style={styles.root}>
-            <GridList
-              cellHeight={'auto'}
-              style={styles.gridList}
-            >
-              {dummyListings.map((listing) => <ListingCard listing={listing}/>)}
-            </GridList>
-          </div>
+        <Container>
+        <Row>
+        <Col sm={12}>
+        <div className="cardsContainer">
+          {dummyListings.map((listing) => <ListingCard listing={listing}/>)}
+        </div>
+        </Col>
+        </Row>
+        </Container>
       </div>
     );
   }
