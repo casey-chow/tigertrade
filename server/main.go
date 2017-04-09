28defb07dd8978146358380d1b1315f59ba7f7db
Casey Chow <casey.chow@gmail.com>
2017-04-08 23:07:36 -0400

    add Sentry support (closes #3)

--- server/main.go
+++ server/main.go
@@ -1,13 +1,14 @@
 package server
 
 import (
+	"github.com/getsentry/raven-go"
 	"github.com/urfave/negroni"
 	"gopkg.in/cas.v1"
 	"net/http"
 	"net/url"
 )
 
-func makeCASMiddleware() negroni.Handler {
+func CASMiddleware() negroni.Handler {
 	casUrl, _ := url.Parse("https://fed.princeton.edu/cas/")
 	casClient := cas.NewClient(&cas.Options{URL: casUrl})
 
@@ -17,11 +18,18 @@ func makeCASMiddleware() negroni.Handler {
 	})
 }
 
+func SentryMiddleware() negroni.Handler {
+	return negroni.HandlerFunc(func(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+		raven.RecoveryHandler(next)(w, r)
+	})
+}
+
 func App() http.Handler {
 	app := negroni.New()
 
-	app.Use(makeCASMiddleware())
-	app.Use(negroni.NewRecovery())
+
+	app.Use(CASMiddleware())
+	app.Use(SentryMiddleware())
 	app.Use(negroni.NewLogger())
 	app.UseHandler(Router())
 
