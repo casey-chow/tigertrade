e2c9707f925ea60954e99e24a9936bf96f82512a
Andrew Wonnacott <awonnacott@princeton.edu>
2017-04-11 01:29:31 -0400

    failing tests, fuck go

--- server/search_test.go
+++ server/search_test.go
@@ -1,29 +1,50 @@
 package server
 
 import (
-//	"fmt"
-//	"github.com/appleboy/gofight"
-	. "github.com/smartystreets/goconvey/convey"
-//	"gopkg.in/h2non/gock.v1"
-//	"net/http"
-	"testing"
+//	"encoding/json"
+	"fmt"
+	"github.com/buger/jsonparser"
+    . "github.com/smartystreets/goconvey/convey"
+	"net/http"
+    "testing"
 )
 
 func TestSearch(t *testing.T) {
-	Convey("When a listing is inserted, a search on a word in its title returns it", t,
-		func() {
+    app := App()
+
+	// Uses hardcoded data from the current testing database
+    Convey("When a listing is inserted,", t, func() {
+        Convey("a search on a word in its title returns it", func() {
+			keyword := "ignore"
+			req, _ := http.NewRequest("GET", fmt.Sprintf("/api/search/%s", keyword), nil)
+			res := executeRequest(app, req)
+
+			So(res.Code, ShouldEqual, http.StatusOK)
+			So(res.Header().Get("Content-Type"), ShouldEqual, "application/json;charset=utf-8")
+			So(isJSON(res.Body.String()), ShouldBeTrue)
+
+			body := []byte(res.Body.String())
+			fUcKgO, _, _, err := jsonparser.Get(body)
+			value := string(fUcKgO)
+			So(err, ShouldBeNil)
+			So(value, ShouldNotBeEmpty)
+			So(value, ShouldHaveLength, 1)
+//			user_id, _, _, err := jsonparser.Get(value[0])
+//			So(v[0]["user_id"], ShouldEqual, 1)
+//			So(v[0]["title"], ShouldContain, keyword)
+//			So(v[0]["description"], ShouldContain, keyword)
+		})
+        Convey("a search on a word in its description returns it", func() {
 			So(nil, ShouldNotBeNil)
 		})
-	Convey("... a search on a word in its description returns it", t,
-		func() {
+		Convey("a search on two words in its title/description returns it exactly once", func() {
 			So(nil, ShouldNotBeNil)
 		})
-	Convey("... a search on two words in its title/description returns it exactly once", t,
-		func() {
+		Convey("searching a word that is not in the title/description will not return it", func() {
 			So(nil, ShouldNotBeNil)
 		})
-	Convey("... searching a word that is not in the title/description will not return it", t,
-		func() {
+		Convey("searching a word that is in multiple listings returns both", func() {
 			So(nil, ShouldNotBeNil)
 		})
+    })
 }
