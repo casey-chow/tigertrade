3dcdc382bb31ddf948d352bb446da5a72c58f429
Casey Chow <casey.chow@gmail.com>
2017-04-12 12:47:38 -0400

    fix tap inject issue in test

--- client/src/containers/App.test.js
+++ client/src/containers/App.test.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './Root';
 
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Root />, div);
