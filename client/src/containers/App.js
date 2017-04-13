11ae9369dda4f4ba64378c468add3a5a7ecb2f1d
Casey Chow <casey.chow@gmail.com>
2017-04-13 12:13:07 -0400

    fix bad merge

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Route } from 'react-router-dom';
+import { Switch, Route, withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import ActionBar from '../components/ActionBar';
 import Home from './Home';
+import Compose from './Compose';
 
 import { loadCurrentUser } from '../actions/users';
 
@@ -21,9 +22,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <ActionBar loading={this.props.loading} user={this.props.user} />
-        <div style={{marginTop: '64px'}}>
-          <Route exact path="/" component={Home} />
+        <ActionBar user={this.props.user} />
+        <div style={{marginTop: '5em'}}>
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route path="/compose" component={Compose}/>
+          </Switch>
         </div>
       </div>
     );
@@ -37,4 +41,4 @@ const mapStateToProps = (state) => {
   });
 };
 
-export default connect(mapStateToProps)(App);
+export default withRouter(connect(mapStateToProps)(App));
