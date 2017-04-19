056d744bf7651a72e3f3ec6c761d44a5a2b9c753
Casey Chow <casey.chow@gmail.com>
2017-04-19 03:16:17 -0400

    allow one listing to be open at a time

--- client/src/components/ListingCard.js
+++ client/src/components/ListingCard.js
@@ -11,34 +11,47 @@ import {
 } from 'material-ui/Card';
 
 class ListingCard extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      expanded: false,
-    };
+
+  static propTypes = {
+    expanded: PropTypes.bool.isRequired,
+    onExpandChange: PropTypes.func,
+    listing: PropTypes.shape({
+      keyId: PropTypes.number,
+      creationDate: PropTypes.string,
+      lastModificationDate: PropTypes.string,
+      title: PropTypes.string,
+      description: PropTypes.string,
+      userId: PropTypes.number,
+      price: PropTypes.number,
+      status: PropTypes.string,
+      expirationDate: PropTypes.number,
+      thumbnail: PropTypes.string,
+    }).isRequired,
+  };
+
+  static defaultProps = {
+    expanded: false,
+    onExpandChange: () => {},
+  };
+
+  state = {
+    expanded: false,
   }
 
   handleExpandChange = (expanded) => {
-    this.setState({ expanded });
-  };
-
-  handleToggle = (event, toggle) => {
-    this.setState({ expanded: toggle });
-  };
-
-  handleExpand = () => {
-    this.setState({ expanded: true });
-  };
-
-  handleReduce = () => {
-    this.setState({ expanded: false });
-  };
+    console.log('ListingCard: expanding to', expanded, this.props.listing.keyId);
+    this.props.onExpandChange(expanded, this.props.listing.keyId);
+  }
 
   render() {
-    const { listing } = this.props;
+    const { listing, expanded } = this.props;
     return (
-      <Card expanded={this.state.expanded} onExpandChange={this.handleExpandChange}>
-        <CardHeader title={listing.title} subtitle={`$${listing.price / 100}`}actAsExpander />
+      <Card expanded={expanded} onExpandChange={this.handleExpandChange}>
+        <CardHeader
+          title={listing.title}
+          subtitle={`$${listing.price / 100}`}
+          actAsExpander
+        />
 
         <CardMedia expandable>
           <img alt={listing.title} src={listing.thumbnail} style={{ minWidth: undefined, maxHeight: '300px', width: 'auto' }} />
@@ -51,20 +64,5 @@ class ListingCard extends React.Component {
     );
   }
 }
-
-ListingCard.propTypes = {
-  listing: PropTypes.shape({
-    keyId: PropTypes.number,
-    creationDate: PropTypes.string,
-    lastModificationDate: PropTypes.string,
-    title: PropTypes.string,
-    description: PropTypes.string,
-    userId: PropTypes.number,
-    price: PropTypes.number,
-    status: PropTypes.string,
-    expirationDate: PropTypes.number,
-    thumbnail: PropTypes.string,
-  }).isRequired,
-};
 
 export default ListingCard;
