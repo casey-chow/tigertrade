6cfaf07f692179a4e800096300cbca35c25d1f8f
Andrew Wonnacott <awonnacott@princeton.edu>
2017-04-23 03:49:08 -0400

    Rename server files to be consistent with endpoints and models

--- server/listings_test.go
+++ server/listings_test.go
@@ -9,6 +9,45 @@ import (
 	"time"
 )
 
+func TestListings(t *testing.T) {
+	app := App()
+
+	Convey("ServeRecentListings", t, func() {
+
+		Convey("should return a list of listings", func() {
+			req, _ := http.NewRequest("GET", "/api/listings", nil)
+			res := executeRequest(app, req)
+
+			So(res.Code, ShouldEqual, http.StatusOK)
+			So(res, shouldHaveContentType, "application/json")
+			So(res.Body.String(), shouldBeJSON)
+		})
+
+	})
+
+	Convey("ServeListingById", t, func() {
+
+		Convey("returns valid JSON", func() {
+			req, _ := http.NewRequest("GET", "/api/listings", nil)
+			res := executeRequest(app, req)
+
+			So(res.Code, ShouldEqual, http.StatusOK)
+			So(res, shouldHaveContentType, "application/json")
+			So(res.Body.String(), shouldBeJSON)
+		})
+
+		Convey("gets the listing with the proper id", nil)
+
+	})
+
+	Convey("ServeAddSeek", t, func() {
+
+		Convey("works given invalid JSON", nil)
+
+	})
+
+}
+
 func TestSearch(t *testing.T) {
 	app := App()
 
--- server/router.go
+++ server/router.go
@@ -10,38 +10,37 @@ func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprint(w, "Welcome!")
 }
 
-func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
-}
-
 // Router returns the router http handler for the package.
 func Router() http.Handler {
 	router := httprouter.New()
 
 	router.GET("/", Index)
-	router.GET("/hello/:name", Hello)
 
-	// Authentication Routes
+	// users.go
 	router.GET("/api/users/redirect", RedirectUser)
 	router.GET("/api/users/logout", LogoutUser)
 	router.GET("/api/users/current", ServeCurrentUser)
 
-	// API Routes
+	// listings.go
 	router.GET("/api/listings", ServeListings)
 	router.POST("/api/listings", ServeAddListing)
 	router.GET("/api/listings/:id", ServeListingById)
 	router.POST("/api/listings/:id", ServeUpdateListingById)
+	//	router.DELETE("/api/listings/:id", ServeDeleteListingById)
 
+	// photos.go
 	router.GET("/api/listings/:id/photos", ServePhotosByListingId)
 	//	router.POST("/api/listings/:id/photos", ServeAddPhoto)
 	//	router.GET("/api/listings/:lid/photos/:pid", ServePhotoById)
 	//	router.POST("/api/listings/:lid/photos/:pid", ServeUpdatePhotoById)
 
+	// seeks.go
 	router.GET("/api/seeks", ServeRecentSeeks)
 	router.POST("/api/seeks", ServeAddSeek)
 	router.GET("/api/seeks/:id", ServeSeekById)
 	//	router.POST("/api/seeks/:id", ServeUpdateSeekById)
 
+	// savedsearches.go
 	router.GET("/api/savedsearches", ServeRecentSavedSearches)
 	router.POST("/api/savedsearches", ServeAddSavedSearch)
 	router.GET("/api/savedsearches/:id", ServeSavedSearchById)
