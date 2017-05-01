6d5276d933f83fa2903423cf57e0e15aca8a1a21
Evan Wildenhain <mew2@princeton.edu>
2017-04-30 20:47:47 -0400

    Could we change it to push "/listings" on a listing submit for parallelism?

--- client/src/containers/Compose.js
+++ client/src/containers/Compose.js
@@ -39,7 +39,7 @@ class Compose extends Component {
       price: data.price ? Math.round(parseFloat(data.price) * 100) : 0,
     }));
     this.props.dispatch(loadListings());
-    this.props.history.push('/');
+    this.props.history.push('/listings');
   }
 
   handleSubmitSeek = (data) => {
