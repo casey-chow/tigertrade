edaa81861bd3fca62854b93dde2c50627f0223a4
Casey Chow <casey.chow@gmail.com>
2017-05-05 01:40:43 -0400

    route to create photo

---
+++ server/images.go
@@ -0,0 +1,45 @@
+package server
+
+import (
+	log "github.com/Sirupsen/logrus"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	// "github.com/casey-chow/tigertrade/server/models"
+	"github.com/getsentry/raven-go"
+	"github.com/julienschmidt/httprouter"
+	"github.com/satori/go.uuid"
+	"net/http"
+)
+
+func CreatePhoto(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	file, fileHeader, err := r.FormFile("file")
+	if err != nil {
+		raven.CaptureError(err, nil)
+		log.WithError(err).Error("error while retrieving photo")
+		Error(w, http.StatusInternalServerError)
+		return
+	}
+	defer file.Close()
+
+	fileUUID := uuid.NewV4().String()
+	path := "/photos/" + fileUUID + "/" + fileHeader.Filename
+
+	res, err := uploader.Upload(&s3manager.UploadInput{
+		Bucket: aws.String("tigertrade"),
+		Key:    aws.String(path),
+		Body:   file,
+	})
+	if err != nil {
+		raven.CaptureError(err, nil)
+		log.WithError(err).Error("error while uploading photo")
+		Error(w, http.StatusInternalServerError)
+		return
+	}
+
+	log.WithField("location", res.Location).Info("successfully uploaded file")
+
+	w.WriteHeader(http.StatusCreated)
+	Serve(w, map[string]string{
+		"location": res.Location,
+	})
+}
--- server/router.go
+++ server/router.go
@@ -45,5 +45,8 @@ func Router() http.Handler {
 	router.PUT("/api/savedsearches/:id", UpdateSavedSearch)
 	router.DELETE("/api/savedsearches/:id", DeleteSavedSearch)
 
+	// images.go
+	router.POST("/api/photos", CreatePhoto)
+
 	return router
 }
